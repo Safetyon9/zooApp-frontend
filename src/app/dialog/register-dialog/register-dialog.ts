dcb5fd28230baa2e89c7b052c2518611e83a0488
Luca <luca@MacBook-Air-di-Luca.local>
2026-03-26 12:20:53 +0100

    register dialog

---
+++ src/app/dialog/register-dialog/register-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-dialog',
+  imports: [],
+  templateUrl: './register-dialog.html',
+  styleUrl: './register-dialog.css',
+})
+export class RegisterDialog {}
