2dd60d4c30922912c1960c40c37539fd1db71d01
carlo <carlogalli05@gmail.com>
2026-03-26 17:16:52 +0100

    modifiche immagini e frecce

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -7,43 +7,7 @@ import { Component, OnDestroy, signal } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home implements OnDestroy{
+export class Home {
 
-    protected readonly title = signal('zooApp-frontend');
-  
-  turtleImages = [
-    'foto/animal1.jpg',
-    'foto/animal2.jpg',
-    'foto/animal3.jpg',
-    'foto/animal4.jpg'
-  ];
-  currentImageIndex = signal(0);
-
-  private intervalId: any;
-
-  constructor() {
-    this.startCarousel();
-  }
-
-  startCarousel() {
-    this.intervalId = setInterval(() => {
-      this.currentImageIndex.update(index => (index + 1) % this.turtleImages.length);
-    }, 5000);
-  }
-
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-
-    nextImage() {
-  this.currentImageIndex.update(index => (index + 1) % this.turtleImages.length);
-}
-
-prevImage() {
-  this.currentImageIndex.update(index => 
-    (index - 1 + this.turtleImages.length) % this.turtleImages.length
-  );
-  }
+    
 }
