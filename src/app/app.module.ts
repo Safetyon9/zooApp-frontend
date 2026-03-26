ea2b2b1b3e55baf2a5549d159ae43f986e3d6dce
safetyon9 <safetyon99@gmaiil.com>
2026-03-26 15:04:03 +0100

    change-password e app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { LoginDialog } from './dialog/login-dialog/login-dialog';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -16,11 +16,15 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { RegisterDialog } from './dialog/register-dialog/register-dialog';
+import { ChangePassword } from './dialog/change-password/change-password';
 
 @NgModule({
   declarations: [
     App,
-    LoginDialog
+    LoginDialog,
+    RegisterDialog,
+    ChangePassword
   ],
   imports: [
     BrowserModule,
