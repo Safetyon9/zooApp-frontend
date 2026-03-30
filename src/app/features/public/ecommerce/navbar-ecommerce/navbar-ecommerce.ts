72fb92eb05ac8e10fde2eae9e53a5882bdf8557d
safetyon9 <safetyon99@gmaiil.com>
2026-03-30 11:56:13 +0200

    altre sezioni commerce

---
+++ src/app/features/public/ecommerce/navbar-ecommerce/navbar-ecommerce.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-ecommerce',
+  imports: [],
+  templateUrl: './navbar-ecommerce.html',
+  styleUrl: './navbar-ecommerce.css',
+})
+export class NavbarEcommerce {}
