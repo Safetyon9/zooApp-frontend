6aff0f04b9658427561c5a9712bc407e8295b434
Luca <luca@MacBook-Air-di-Luca.local>
2026-03-30 11:40:21 +0200

    navbar utente

---
+++ src/app/features/user/navbar-utente/navbar-utente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-utente',
+  imports: [],
+  templateUrl: './navbar-utente.html',
+  styleUrl: './navbar-utente.css',
+})
+export class NavbarUtente {}
