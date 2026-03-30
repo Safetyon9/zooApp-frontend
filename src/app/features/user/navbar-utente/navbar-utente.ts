fe8312687da1a83c17670e96166d668c817418f0
Luca <luca@MacBook-Air-di-Luca.local>
2026-03-30 14:28:03 +0200

    Gestione Utenti

--- src/app/features/user/navbar-utente/navbar-utente.ts
+++ src/app/features/user/navbar-utente/navbar-utente.ts
@@ -3,7 +3,10 @@ import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
 import { ChangePassword } from '../../auth/dialog/change-password/change-password';
+import { UtenteServices } from '../services/utente-services';
+import { RegisterDialog } from '../../auth/dialog/register-dialog/register-dialog';
 import { AuthServices } from '../../../core/services/auth-services';
+import { Utilities } from '../../../core/utils/utilities';
 
 @Component({
   selector: 'app-navbar-utente',
@@ -15,11 +18,13 @@ export class NavbarUtenteComponent {
   constructor(
     public auth: AuthServices,
     private router: Router,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private utenteServices: UtenteServices,
+    private util: Utilities
   ) {}
 
   goHome(): void {
-    this.router.navigate(['/utente']);
+    this.router.navigate(['']);
   }
 
   changePWD(): void {
@@ -28,8 +33,27 @@ export class NavbarUtenteComponent {
     });
   }
 
+
   logout(): void {
     this.auth.resetAll();
-    this.router.navigate(['/utente']);
+    this.router.navigate(['']);
+  }
+
+  profile(): void {
+    const userId = this.auth.grant()?.userId;
+    if (!userId) return;
+
+    this.utenteServices.findByUserName(userId).subscribe({
+      next: (r: any) => {
+        this.util.openDialog(
+          RegisterDialog,
+          { account: r, mode: 'U' },
+          { width: '90vw', maxWidth: '1200px', height: 'auto' }
+        );
+      },
+      error: (err: any) => {
+        console.error('error getAccount:', err);
+      }
+    });
   }
 }
