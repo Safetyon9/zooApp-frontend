f2b34de9205e0e6a379598d35405ad89fe4181e0
Luca <luca@MacBook-Air-di-Luca.local>
2026-04-07 11:54:32 +0200

    admin navbar uguale a navbar utente

--- src/app/features/admin/admin-navbar/admin-navbar.ts
+++ src/app/features/admin/admin-navbar/admin-navbar.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
+import { AuthServices } from '../../../core/services/auth-services';
 @Component({
   selector: 'app-admin-navbar',
   imports: [],
   templateUrl: './admin-navbar.html',
   styleUrl: './admin-navbar.css',
 })
-export class AdminNavbar {}
+
+export class AdminNavbar {
+  @Output() profileSelected = new EventEmitter<void>();
+  @Output() homeSelected = new EventEmitter<void>();
+
+  constructor(
+    public auth: AuthServices,
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
+
+  goHome(): void {
+    this.homeSelected.emit();
+  }
+
+  openProfile(): void {
+    this.profileSelected.emit();
+  }
+
+  changePWD(): void {
+    this.router.navigate(['']);
+  }
+
+  logout(): void {
+    this.auth.resetAll();
+    this.router.navigate(['']);
+  }
+}
