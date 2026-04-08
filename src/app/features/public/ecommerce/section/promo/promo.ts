20306930ee1a74d5287493780ffd0f717fd4cd3c
Ramona <riolo628@gmail.com>
2026-04-08 11:18:00 +0200

    negozio

--- src/app/features/public/ecommerce/section/promo/promo.ts
+++ src/app/features/public/ecommerce/section/promo/promo.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-promo',
@@ -6,4 +6,48 @@ import { Component } from '@angular/core';
   styleUrl: './promo.css',
   standalone: false,
 })
-export class Promo {}
+export class Promo implements OnInit, OnDestroy {
+  images = [
+    'foto/shop1.webp',
+    'foto/shop2.webp',
+    'foto/shop3.avif',
+    'foto/shop4.avif'
+  ];
+  
+  currentIndex = signal<number>(0);
+  intervalId: any;
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  ngOnDestroy() {
+    this.stopCarousel();
+  }
+
+  startCarousel() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  stopCarousel() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  nextSlide() {
+    this.currentIndex.update(index => (index + 1) % this.images.length);
+  }
+
+  prevSlide() {
+    this.currentIndex.update(index => (index - 1 + this.images.length) % this.images.length);
+  }
+
+  setSlide(index: number) {
+    this.currentIndex.set(index);
+    this.stopCarousel();
+    this.startCarousel();
+  }
+}
