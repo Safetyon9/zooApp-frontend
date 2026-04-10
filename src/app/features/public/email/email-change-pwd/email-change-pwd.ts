1f1e0e150900c448f90c65ecb1b26f0755aa88df
Luca <luca@MacBook-Air-di-Luca.local>
2026-04-10 13:48:23 +0200

    public email files

---
+++ src/app/features/public/email/email-change-pwd/email-change-pwd.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-change-pwd',
+  imports: [],
+  templateUrl: './email-change-pwd.html',
+  styleUrl: './email-change-pwd.css',
+})
+export class EmailChangePwd {}
